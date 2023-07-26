022c27dccb2ad03d96245281ec1b2f3318bf78b2
AJAO.H <hazzanaj@gmail.com>
2023-07-26 13:00:49 +0300

    Added first stage dependencies

---
+++ fleetmanagement-backend/src/main/java/com/fleetmanagementapp/demo/HajaoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.fleetmanagementapp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HajaoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HajaoProjectApplication.class, args);
+	}
+
+}
---
+++ fleetmanagement-backend/src/test/java/com/fleetmanagementapp/demo/HajaoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fleetmanagementapp.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HajaoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
