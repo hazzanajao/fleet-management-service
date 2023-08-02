104676779bfc15a3d194e80c461005005ffb6692
AJAO.H <hazzanaj@gmail.com>
2023-08-02 08:55:22 +0300

    Added AuthRequest and AuthResponse

---
+++ fleetmanagement-backend/src/main/java/com/fleetmanagementapp/model/AuthResponse.java
@@ -0,0 +1,15 @@
+package com.fleetmanagementapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+    private User user;
+    private Map<String,String> token;
+}
